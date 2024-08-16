36fbebb2f8c94b6573ba0a143482e153ee7db4d8
EnriqueColpo <EnriqueColpo@gmail.com>
2024-08-16 20:06:34 -0300

    🧱 | fix api url

--- services/api/main.py
+++ services/api/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://switchshop.world"],
+    allow_origins=[os.getenv("ALLOWED_ORIGINS")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
